3f3c087d80e322ee1f13db2251c6e556f308103c
Astonark <sorel.kyrian@gmail.com>
2023-04-02 18:55:15 +0200

    move home page to /

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,7 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/home.tsx";
+import * as $3 from "./routes/count.tsx";
 import * as $4 from "./routes/index.tsx";
 import * as $5 from "./routes/resume.tsx";
 import * as $$0 from "./islands/Counter.tsx";
@@ -16,7 +16,7 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/home.tsx": $3,
+    "./routes/count.tsx": $3,
     "./routes/index.tsx": $4,
     "./routes/resume.tsx": $5,
   },
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,49 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { Button } from "../components/Button.tsx";
+import { Footer } from "../components/Footer.tsx";
+import { Navbar } from "../components/Navbar.tsx";
+
+// routes/about.tsx
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
-    </>
-  );
-}
+    return (
+        <div>
+            <header>
+                <Navbar></Navbar>
+                <div class="flex justify-center mt-16">
+                    <div class="mt-12 pt-20">
+                        <h2 class="text-9xl font-bold">Maëlly <br/> Sorel</h2>
+                        <h1 class="text-5xl mb-3">Designeuse graphique</h1>
+                        <Button>Mes réalisations</Button>
+                        <ul class="flex mt-3">
+                            <li><img src="/behance.png" alt="Behance" width="48" height="48" class="mr-5"/></li>
+                            <li><img src="/linkedin.png" alt="LinkedIn" width="48" height="48" class="mr-5"/></li>
+                            <li><img src="/instagram.png" alt="Instagram" width="48" height="48"/></li>
+                        </ul>
+                    </div>
+                    <img src="/banner_logo.png" alt="" />
+                </div>
+            </header>
+
+            <main class="flex justify-center items-center mt-32">
+                <img src="/profil.png" alt="Photo de profil" class="mr-20"/>
+                <span class="w-1/3">
+                    <h2 class="text-5xl mb-6">À propos de moi</h2>
+                    <p class="text-2xl mb-6">Salut et bienvenue sur mon site &nbsp;! <br/>
+                        Moi c'est Maëlly et je suis une jeune designeuse graphique encore en formation. <br/>
+                        Je suis également auto entrepreneuse depusi peu spécialiser dans l'UX/UI et le branding. <br/>
+                        Vous retrouverez ici ce dont je suis capable, ce que j'ai à vous proposer et ce que j'ai déjà pu réaliser. <br/>
+                        A plus tard !!
+                    </p>
+                    <Button><a href="#">Mon CV</a></Button>
+                </span>
+            </main>
+
+            <section class="mt-40">
+                <h2 class="text-center text-6xl font-semibold mb-6">Mes réalisations</h2>
+                <img class="mx-auto" src="/reals.png" alt="" />
+                <div class="text-center mt-6"><Button>En savoir plus</Button></div>
+            </section>
+            <Footer></Footer>
+        </div>
+    );
+  }
