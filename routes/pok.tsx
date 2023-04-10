fe7d3ca7052409f1f65d69afc1c23cb1d09b3493
Astonark <sorel.kyrian@gmail.com>
2023-04-10 17:53:42 +0200

    pok page desktop

--- routes/pok.tsx
+++ routes/pok.tsx
@@ -13,13 +13,14 @@ export default function Pok() {
             <main class="flex flex-col justify-center items-center mt-12 md:mt-32">
                 <img src="/images/projects/pok/logo.png" alt="" class=""/>
                 <div class="flex mt-12">
-                    <img src="/images/projects/pok/pok.png" alt="" />
-                    <img src="/images/projects/pok/qr.png" alt="" />
+                    <img src="/images/projects/pok/pok.png" alt="" class="md:hidden"/>
+                    <img src="/images/projects/pok/qr.png" alt="" class="md:hidden" />
+                    <img src="/images/projects/pok/group.png" class="hidden md:block" />
                 </div>
-                <div class="text-center">
-                    <span class="text-left inline-block mx-6 md:w-1/3">
-                        <h2 class="mt-6 text-xl mx-6 md:text-5xl mb-3 md:mb-6">Création d'une agence de design, POK&nbsp;: </h2>
-                        <ul class="text-s mx-6 list-disc list-inside mb-6">
+                <div class="text-center text-left">
+                    <span class="text-left inline-block mx-6">
+                        <h2 class="mt-6 text-xl mx-6 md:text-5xl mb-3 md:mb-6 font-['Lexend']">Création d'une agence de design, POK&nbsp;: </h2>
+                        <ul class="text-s mx-6 list-disc list-inside mb-6 font-['Lexend']">
                             <li>Définition d'un besoin et de la cible de l'agence</li>
                             <li>Création de l'identité de l'entreprise</li>
                             <li>Site internet</li>
@@ -29,9 +30,10 @@ export default function Pok() {
                     </span>
                 </div>
 
-                <img src="/images/projects/pok/poster.png" alt="" class=""/>
-                <img src="/images/projects/pok/banner.png" alt="" class="my-3"/>
-                <img src="/images/projects/pok/contact.png" alt="" class=""/>
+                <img src="/images/projects/pok/poster.png" alt="" class="md:hidden"/>
+                <img src="/images/projects/pok/banner.png" alt="" class="my-3 md:hidden"/>
+                <img src="/images/projects/pok/contact.png" alt="" class="md:hidden"/>
+                <img src="/images/projects/pok/pages.png" alt="" class="hidden md:block"/>
             </main>
             <Footer></Footer>
         </>
