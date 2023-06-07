4da70711cb7284e5a61542cc8e6b7c4bd77c1ebf
Astonark <sorel.kyrian@gmail.com>
2023-06-07 13:53:13 +0200

    test

--- routes/about.tsx
+++ routes/about.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "../components/Navbar.tsx";
 export default function About() {
     return (
         <div>
+            <p>Salut tout le monde</p>
             <Navbar></Navbar>
            <img class="m-auto md:hidden" src="images/resume.png" alt="mon cv" />
            <img class="m-auto hidden md:block" src="images/resume_desktop.png" alt="mon cv" />
