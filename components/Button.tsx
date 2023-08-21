7eaa599424ec784163545c79cbaa9c672c070f27
Astonark <sorel.kyrian@gmail.com>
2023-08-21 21:35:38 +0200

    added custom dark green color for the button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,8 +6,8 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 rounded-full px-5 font-medium text-xs md:text-2xl font-['Lexend']"
-      style="background-color: #ACBCB1; color: #4F7158"
+      class="px-2 py-1 rounded-full px-5 font-bold text-base md:text-2xl font-['Lexend'] text-dark-green"
+      style="background-color: #ACBCB1;"
     />
   );
 }
--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,11 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    extend: {
+      colors: {
+        'dark-green': '#26462F',
+      },
+    }
+  }
 } as Options;
